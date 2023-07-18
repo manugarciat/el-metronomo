e7270c7970b0b38f83e7dcaf418e34fcb65d1475
Manuel García Tornadú <manuelgarciatornadu@gmail.com>
2023-07-18 18:21:58 -0300

    Inicia audiotrack cuando ejecuta metronomo y libera memoria al apretar stop. Agrega icono de la app.

--- app/src/main/java/com/mnlgt/elmetronomo/MetronomoAudioTrack.kt
+++ app/src/main/java/com/mnlgt/elmetronomo/MetronomoAudioTrack.kt
@@ -12,14 +12,15 @@ import kotlin.math.sin
 
 class MetronomoAudioTrack {
 
-    //audiotrack
-    private var _audioTrack: AudioTrack = obtenerAudioTrack()
-
-    //cantidad de samples que vamos a imprimir en cada bloque, igual al buffer size
-    private var _numSamples: Int = _audioTrack.bufferSizeInFrames
-
-    //array donde guardamos los samples calculados para escribir en audiotrack, valor entero de 16 bit (short, 2 bytes)
-    private var _generatedSnd: ShortArray = ShortArray(_numSamples)
+    //quedó desordenado
+//    //audiotrack
+//    private var _audioTrack
+//
+//    //cantidad de samples que vamos a imprimir en cada bloque, igual al buffer size
+//    private var _numSamples: Int
+//
+//    //array donde guardamos los samples calculados para escribir en audiotrack, valor entero de 16 bit (short, 2 bytes)
+//    private var _generatedSnd
 
     //variables para la escritura del audio, lleva la cantidad de samples impresos del tick y del silencio
     private var sndPosition = 0
@@ -44,7 +45,12 @@ class MetronomoAudioTrack {
 
 
     //genera sample para reproducir, el largo es en cantidad de samples, devuelve array de numeros entre -1 y 1 (Double)
-    private fun generateSample(largo: Int, frecuencia: Double, volumen: Double = 1.0): DoubleArray {
+    private fun generateSample(
+        largo: Int,
+        frecuencia: Double,
+        volumen: Double = 1.0,
+        audioTrack: AudioTrack
+    ): DoubleArray {
 
         val fadeIn = 100
         val fadeOut = 2000
@@ -55,17 +61,17 @@ class MetronomoAudioTrack {
             when (i) {
                 in 0 until fadeIn -> { //fade in
                     result[i] =
-                        sin(2 * Math.PI * i / (_audioTrack.sampleRate / frecuencia)) * (i / fadeIn.toDouble()) * volumenFactor * volumen
+                        sin(2 * Math.PI * i / (audioTrack.sampleRate / frecuencia)) * (i / fadeIn.toDouble()) * volumenFactor * volumen
                 }
 
                 in fadeIn until largo - fadeOut -> {
                     result[i] =
-                        sin(2 * Math.PI * i / (_audioTrack.sampleRate / frecuencia)) * volumenFactor * volumen
+                        sin(2 * Math.PI * i / (audioTrack.sampleRate / frecuencia)) * volumenFactor * volumen
                 }
 
                 in largo - fadeOut until largo -> { //fade out
                     result[i] =
-                        sin(2 * Math.PI * i / (_audioTrack.sampleRate / frecuencia)) * ((largo - i) / fadeOut.toDouble()) * volumenFactor * volumen
+                        sin(2 * Math.PI * i / (audioTrack.sampleRate / frecuencia)) * ((largo - i) / fadeOut.toDouble()) * volumenFactor * volumen
                 }
             }
         }
@@ -96,8 +102,12 @@ class MetronomoAudioTrack {
     }
 
     //calcula silencios para todas las subdivisiones:
-    private fun calcularSilencio(duracioSonido: Int, duracionSubdivision: Int): IntArray {
-        val duracionBeat = ((60.0 / _tempo) * _audioTrack.sampleRate).toInt()
+    private fun calcularSilencio(
+        duracioSonido: Int,
+        duracionSubdivision: Int,
+        audioTrack: AudioTrack
+    ): IntArray {
+        val duracionBeat = ((60.0 / _tempo) * audioTrack.sampleRate).toInt()
         val resultado = IntArray(5)
         for (i in resultado.indices) {
             if (i != 0)
@@ -109,6 +119,15 @@ class MetronomoAudioTrack {
 
     suspend fun iniciar() {
 
+        //audiotrack
+        val audioTrack: AudioTrack = obtenerAudioTrack()
+
+        //cantidad de samples que vamos a imprimir en cada bloque, igual al buffer size
+        val numSamples: Int = audioTrack.bufferSizeInFrames
+
+        //array donde guardamos los samples calculados para escribir en audiotrack, valor entero de 16 bit (short, 2 bytes)
+        val generatedSnd: ShortArray = ShortArray(numSamples)
+
         sndPosition = 0
         silPosition = 0
         beatPosition = 1
@@ -117,38 +136,50 @@ class MetronomoAudioTrack {
         beginStop = false
         detenerAhora = false
 
-        val tick16bit = to16Bit(generateSample(_audioTrack.sampleRate / 16, 360.0, 0.7))
-        val acento16bit = to16Bit(generateSample(_audioTrack.sampleRate / 16, 432.0))
-        val subSonido16Bit = to16Bit(generateSample(_audioTrack.sampleRate / 16, 360.0, 0.2))
+        val tick16bit = to16Bit(generateSample(audioTrack.sampleRate / 16, 360.0, 0.7, audioTrack))
+        val acento16bit = to16Bit(generateSample(
+            audioTrack.sampleRate / 16,
+            432.0,
+            audioTrack = audioTrack
+        ))
+        val subSonido16Bit = to16Bit(generateSample(audioTrack.sampleRate / 16, 360.0, 0.2, audioTrack))
 
-        _audioTrack.play()
+        audioTrack.play()
 
         withContext(Dispatchers.Default) {
             while (!detenerAhora) {
 
-                imprimirAudio(tick16bit, acento16bit, subSonido16Bit)
-                _audioTrack.write(_generatedSnd, 0, _generatedSnd.size)
+                imprimirAudio(tick16bit, acento16bit, subSonido16Bit, generatedSnd, audioTrack)
+                audioTrack.write(generatedSnd, 0, generatedSnd.size)
             }
 
-            _audioTrack.stop()
-            //_audioTrack.release()
+            audioTrack.pause()
+            audioTrack.flush()
+            audioTrack.stop()
+            audioTrack.release()
         }
     }
 
-    private fun imprimirAudio(tick: ShortArray, acento: ShortArray, subS: ShortArray) {
+    private fun imprimirAudio(
+        tick: ShortArray,
+        acento: ShortArray,
+        subS: ShortArray,
+        generatedSnd: ShortArray,
+        audioTrack: AudioTrack
+    ) {
 
-        val silencio = calcularSilencio(tick.size, subS.size)
+        val silencio = calcularSilencio(tick.size, subS.size, audioTrack)
 
-        for (i in _generatedSnd.indices) {
+        for (i in generatedSnd.indices) {
 
             val sonido = if (subPosition != 1) subS else
                 if (beatPosition == 1) acento else tick
 
             if (sndPosition < sonido.size) {
-                _generatedSnd[i] = sonido[sndPosition]
+                generatedSnd[i] = sonido[sndPosition]
                 sndPosition++
             } else {
-                _generatedSnd[i] = 0
+                generatedSnd[i] = 0
                 silPosition++
 
                 if (silPosition >= silencio[_subdivision]) {
@@ -167,11 +198,11 @@ class MetronomoAudioTrack {
             if (beginStop) {
                 if (fadePosition >= fadeLength) {
                     detenerAhora = true
-                    _generatedSnd[i] = 0
+                    generatedSnd[i] = 0
                 } else {
                     //genera fade out:
-                    _generatedSnd[i] =
-                        (_generatedSnd[i] * ((fadeLength - fadePosition) / fadeLength.toFloat())).toInt()
+                    generatedSnd[i] =
+                        (generatedSnd[i] * ((fadeLength - fadePosition) / fadeLength.toFloat())).toInt()
                             .toShort()
                     fadePosition++
                 }
